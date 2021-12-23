cc0dd9b997c69d31e4ed7d0b0cc152344c32b6d6
pososix <74709275+pososix@users.noreply.github.com>
2021-12-23 16:29:50 +0300

    Delete sem1/zlobin/task4_var0 directory

--- sem1/zlobin/task4_var0/factorial.h
+++
@@ -1,8 +0,0 @@
-#ifndef FACTORIAL_H_INCLUDED
-#define FACTORIAL_H_INCLUDED
-
-long double cycle(long double n);
-long double recursion(long double n);
-long double stirling(long double n);
-
-#endif // FACTORIAL_H_INCLUDED
